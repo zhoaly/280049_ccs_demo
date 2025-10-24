2e70bf22cb4c523ddb04c3d76c21f9671fe2cde5
zhoaly <124871020+zhoaly@users.noreply.github.com>
2025-10-24 08:48:12 +0800

    feat: add app and driver module scaffolds

---
+++ APP/module1/app_module1.c
@@ -0,0 +1,11 @@
+#include "app_module1.h"
+
+void app_module1_init(void)
+{
+    // TODO: 初始化应用子模块1所需的资源
+}
+
+void app_module1_process(void)
+{
+    // TODO: 添加应用子模块1的核心处理逻辑
+}
---
+++ DRV/include/drv_driver2.h
@@ -0,0 +1,7 @@
+#ifndef DRV_DRIVER2_H
+#define DRV_DRIVER2_H
+
+void drv_driver2_init(void);
+void drv_driver2_control(void);
+
+#endif /* DRV_DRIVER2_H */
