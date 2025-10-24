79a80ed316ec917a96bacef374fa36a23c480375
zhaoly <1328124580@qq.com>
2025-10-24 13:54:34 +0800

    整理文件系统结构

---
+++ CODE/APP/include/app_module2.h
@@ -0,0 +1,7 @@
+#ifndef APP_MODULE2_H
+#define APP_MODULE2_H
+
+void app_module2_init(void);
+void app_module2_execute(void);
+
+#endif /* APP_MODULE2_H */
---
+++ CODE/DRV/include/drv_driver1.h
@@ -0,0 +1,7 @@
+#ifndef DRV_DRIVER1_H
+#define DRV_DRIVER1_H
+
+void drv_driver1_init(void);
+void drv_driver1_update(void);
+
+#endif /* DRV_DRIVER1_H */
